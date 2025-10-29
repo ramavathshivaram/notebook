f1c08ada40efc4d9156d400efed8e1f8845cd985
RAMAVTH SHIVA RAM <ramavathshiva6300@gmail.com>
2025-10-29 08:03:31 +0530

    Update email transporter configuration and sender format

--- backend/config/nodeMailler.js
+++ backend/config/nodeMailler.js
@@ -1,9 +1,9 @@
 const nodeMailer = require("nodemailer");
 
 const transporter = nodeMailer.createTransport({
-  service: "gmail",
   host: "smtp-relay.brevo.com",
   port: 587,
+  secure: false,
   auth: {
     user: process.env.EMAIL,
     pass: process.env.PASSWORD,
@@ -12,7 +12,7 @@ const transporter = nodeMailer.createTransport({
 
 const sentEmail = async (to, sub, text) => {
   await transporter.sendMail({
-    from: process.env.EMAIL,
+    from: `"AI NOTEBOOK" <${process.env.EMAIL}>`,
     to: to,
     subject: sub,
     html: text,
