304cb720a9236e2f362bbcb614a1c4eb6d82ccea
ramavathshivaram <ramavathshiva6300@gmail.com>
2026-05-14 12:47:19 +0530

    Update email configuration to use Brevo service and add related environment variables

--- backend/config/nodeMailler.js
+++ backend/config/nodeMailler.js
@@ -1,16 +1,18 @@
 const nodeMailer = require("nodemailer");
 
 const transporter = nodeMailer.createTransport({
-  service: "gmail",
+  host: process.env.BREVO_SERVER,
+  port: process.env.BREVO_PORT,
+  secure: false, // true for 465, false for 587
   auth: {
-    user: process.env.EMAIL,
-    pass: process.env.PASSWORD,
+    user: process.env.BREVO_USER,
+    pass: process.env.BREVO_API_KEY,
   },
 });
 
 const sentEmail = async (to, sub, text) => {
   await transporter.sendMail({
-    from: `"AI NOTEBOOK" <${process.env.EMAIL}>`,
+    from: `"AI NOTEBOOK" <${process.env.BREVO_USER}>`,
     to: to,
     subject: sub,
     html: text,
